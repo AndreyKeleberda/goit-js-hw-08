2846a8b53db17b80da2b1be078f79a6def361fc1
AndreyKeleberda <a.keleberda@ukr.net>
2022-02-16 23:12:23 +0200

    add task2 solution

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const KEY_TIME_VIDEO = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(KEY_TIME_VIDEO, JSON.stringify(data));
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+document.addEventListener('DOMContentLoaded', reload);
+function reload() {
+  player
+    .setCurrentTime(JSON.parse(localStorage.getItem(KEY_TIME_VIDEO)).seconds)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+        default:
+          break;
+      }
+    });
+}
